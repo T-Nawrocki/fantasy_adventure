16644623e08d3d507223d72b6995ee4722da5ef7
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-05 11:36:19 +0100

    wraith class added and passes all tests

---
+++ src/main/java/enemy/Wraith.java
@@ -0,0 +1,15 @@
+package enemy;
+
+public class Wraith extends Enemy {
+
+    public Wraith() {
+        super(
+                "Wraith",
+                "a dark cloud of mist in vaguely human shape, " +
+                        "with sharp claws and a shriveled, hateful face",
+                60,
+                20
+        );
+    }
+
+}
---
+++ src/test/java/enemy/WraithTest.java
@@ -0,0 +1,53 @@
+package enemy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WraithTest {
+
+    private Wraith wraith;
+
+    @Before
+    public void before() {
+        wraith = new Wraith();
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Wraith", wraith.getName());
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals(
+                "a dark cloud of mist in vaguely human shape, " +
+                        "with sharp claws and a shriveled, hateful face",
+                wraith.getDescription()
+        );
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(60, wraith.getHealth());
+    }
+
+    @Test
+    public void hasStrength() {
+        assertEquals(20, wraith.getStrength());
+    }
+
+    @Test
+    public void canLoseHealth() {
+        wraith.loseHealth(1);
+        assertEquals(59, wraith.getHealth());
+    }
+
+    @Test
+    public void cannotGoBelow0Health() {
+        wraith.loseHealth(10000);
+        assertEquals(0, wraith.getHealth());
+    }
+
+}
