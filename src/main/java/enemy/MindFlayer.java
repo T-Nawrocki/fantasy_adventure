fca3fa1e46610d32afbe5e496c1e5e175b2beea2
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-05 11:47:14 +0100

    mind flayer class added and passes tests

---
+++ src/main/java/enemy/MindFlayer.java
@@ -0,0 +1,15 @@
+package enemy;
+
+public class MindFlayer extends Enemy {
+
+    public MindFlayer() {
+        super(
+                "Mind Flayer",
+                "a cruel-looking humanoid with tentacles where its mouth should be, " +
+                        "and a psychic aura surrounding it",
+                80,
+                25
+        );
+    }
+
+}
---
+++ src/test/java/enemy/MindFlayerTest.java
@@ -0,0 +1,53 @@
+package enemy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MindFlayerTest {
+
+    private MindFlayer mindFlayer;
+
+    @Before
+    public void before() {
+        mindFlayer = new MindFlayer();
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Mind Flayer", mindFlayer.getName());
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals(
+                "a cruel-looking humanoid with tentacles where its mouth should be, " +
+                        "and a psychic aura surrounding it",
+                mindFlayer.getDescription()
+        );
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(80, mindFlayer.getHealth());
+    }
+
+    @Test
+    public void hasStrength() {
+        assertEquals(25, mindFlayer.getStrength());
+    }
+
+    @Test
+    public void canLoseHealth() {
+        mindFlayer.loseHealth(1);
+        assertEquals(79, mindFlayer.getHealth());
+    }
+
+    @Test
+    public void cannotGoBelow0Health() {
+        mindFlayer.loseHealth(10000);
+        assertEquals(0, mindFlayer.getHealth());
+    }
+
+}
