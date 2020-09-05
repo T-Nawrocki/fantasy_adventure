822d565a60fbbcd585c587a2e52fe32adc20d782
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-05 11:12:50 +0100

    hobgoblin enemy class added

---
+++ src/main/java/enemy/Hobgoblin.java
@@ -0,0 +1,14 @@
+package enemy;
+
+public class Hobgoblin extends Enemy {
+
+    public Hobgoblin() {
+        super(
+                "Hobgoblin",
+                "a large goblin-like creature that's covered in fur, holding a spiked mace",
+                40,
+                15
+        );
+    }
+
+}
---
+++ src/test/java/enemy/HobgoblinTest.java
@@ -0,0 +1,52 @@
+package enemy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HobgoblinTest {
+
+    private Hobgoblin hobgoblin;
+
+    @Before
+    public void before() {
+        hobgoblin = new Hobgoblin();
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Hobgoblin", hobgoblin.getName());
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals(
+                "a large goblin-like creature that's covered in fur, holding a spiked mace",
+                hobgoblin.getDescription()
+        );
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(40, hobgoblin.getHealth());
+    }
+
+    @Test
+    public void hasStrength() {
+        assertEquals(15, hobgoblin.getStrength());
+    }
+
+    @Test
+    public void canLoseHealth() {
+        hobgoblin.loseHealth(1);
+        assertEquals(39, hobgoblin.getHealth());
+    }
+
+    @Test
+    public void cannotGoBelow0Health() {
+        hobgoblin.loseHealth(10000);
+        assertEquals(0, hobgoblin.getHealth());
+    }
+
+}
